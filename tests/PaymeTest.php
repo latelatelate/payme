e6eeb63fcc4374eacea8842bc341b34fc551d3fa
Nick Mow <nickmow@gmail.com>
2015-09-29 15:31:17 -0400

    Disabled test class for now. Update later

--- tests/PaymeTest.php
+++ tests/PaymeTest.php
@@ -2,12 +2,12 @@
  
 use NM\Payme\Payme;
  
-class PaymeTest extends PHPUnit_Framework_TestCase {
- 
-  public function testNachHasCheese()
+//class PaymeTest extends PHPUnit_Framework_TestCase {
+class PaymeTest {
+  /*public function testNachHasCheese()
   {
     $nacho = new Payme;
     $this->assertTrue($nacho->hasCheese());
-  }
+  }*/
  
 }
